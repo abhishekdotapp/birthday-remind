9bafc96b60e90352789c653a53c1e0ee2ee5e228
Abhishek <abhishekjaiswal07890@gmail.com>
2025-09-07 17:49:18 +0530

    deployment config set

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+type CustomNextConfig = {
+    output?: 'standalone' | 'static' | 'export';
+    // Include other Next.js config properties as needed
+    reactStrictMode?: boolean;
+    // ... other properties
+};
+
+
+const nextConfig: CustomNextConfig = {
+    output: "static",
 };
 
 export default nextConfig;
